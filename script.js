3ed929d67a899a0b40250692e7e5fd2daf04b625
harika <harika@actimize.in>
2026-08-20 17:35:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
+
+function showMessage() {
+  alert("Welcome! Let's build something amazing.");
+}
